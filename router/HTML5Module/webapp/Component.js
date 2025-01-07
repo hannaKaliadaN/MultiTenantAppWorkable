1544bddbecc142d5a6777359b19efd0dcaba5be2
Anna Kaliada <hanna.kaliada.leverx@gmail.com>
2025-01-07 07:17:11 +0000

    Delete unneccesary files

--- router/HTML5Module/webapp/Component.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/ui/core/UIComponent"],function(t){"use strict";return t.extend("HTML5Module.Component",{metadata:{manifest:"json"},init:function(){t.prototype.init.apply(this,arguments);this.getRouter().initialize()}})});
-//# sourceMappingURL=Component.js.map
--- router/HTML5Module/webapp/controller/App-dbg.controller.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (BaseController) {
-    "use strict";
-
-    return BaseController.extend("HTML5Module.controller.App", {
-        onInit() {},
-    });
-});
